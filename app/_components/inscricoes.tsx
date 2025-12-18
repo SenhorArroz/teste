d47367078f7b214df17f5350fa30e72d35a3f0da
SenhorArroz <luizrob.guimaraes@hotmail.com>
2025-12-17 21:45:10 -0300

    adicao de componentes qeum pode e inscricoes

--- app/_components/inscricoes.tsx
+++ app/_components/inscricoes.tsx
@@ -138,7 +138,7 @@ export default function Inscricoes() {
                 </div>
 
                 <h2 className="text-2xl font-bold text-white text-center mb-8">
-                    Cursos Disponíveis (84 vagas totais)
+                    Cursos Disponíveis (? vagas totais)
                 </h2>
 
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-6 lg:gap-8 mb-16">
